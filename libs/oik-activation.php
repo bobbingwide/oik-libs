9d72158a414ba6f92fe9d644d1ddde65141fb2f0
Herb Miller <herb@bobbingwide.com>
2016-04-02 18:06:48 +0100

    Synchronize with oik

--- libs/oik-activation.php
+++ libs/oik-activation.php
@@ -1,4 +1,4 @@
-<?php // (C) Copyright Bobbing Wide 2012-2015
+<?php // (C) Copyright Bobbing Wide 2012-2016
 
 if ( function_exists( "oik_plugin_lazy_activation" ) ) {
  // It's already defined so we don't need this lot
@@ -30,14 +30,15 @@ function oik_plugin_install_plugin( $plugin ) {
  * We may not be activating the main plugin, so we need the relative path filename of the plugin to activate
  * @return string link to enable activation - which user must choose
  * We probably don't need plugin_status OR paged parameters
- 
- 
+ *
+ * `
    http://example.com/wp-admin/plugins.php?
      action=activate
      &plugin=oik%2Foik.php
      &plugin_status=all
      &paged=1&s
      &_wpnonce=a53a158be5
+ * `
 */                              
 function oik_plugin_activate_plugin( $plugin, $plugin_name) {
   $path = "plugins.php?action=activate&plugin_status=all&paged=1&s&plugin=$plugin";
@@ -51,7 +52,10 @@ function oik_plugin_activate_plugin( $plugin, $plugin_name) {
   return( $link );
 } 
  
-/**    
+/**
+ * Create an Upgrade plugin link
+ *
+     
     $path = "update.php?action=install-plugin&plugin=$plugin";
     $url = admin_url( $path );
     $url = wp_nonce_url( $url, "install-plugin_oik" ); 
@@ -75,6 +79,7 @@ function oik_plugin_update_plugin( $plugin ) {
 
 /** 
  * Find out if we think the plugin is installed but not activated or not even installed
+ * 
  * @param string $plugin - the plugin file name ( without plugin path info? )
  * @return string - null if it's not installed or plugin to be activated
  C:\apache\htdocs\wordpress\wp-content\plugins\oik\shortcodes\oik-bob-bing-wide.php(289:0) 2012-05-23T07:52:15+00:00 696 cf=the_content bw_get_plugins(4)  Array
@@ -119,6 +124,8 @@ function oik_plugin_check_installed_plugin( $plugin ) {
 
 
 /**
+ * Test if the plugin is activated
+ *
  * This won't work for Multisite since it doesn't find the network activated plugins
  * Even if it did, the admin may not be able to do anything.
 */
